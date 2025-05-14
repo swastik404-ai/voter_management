73b77af1fa115c679f4969f9e998320438efd7d2
swastik404-ai <r4m4n666666@gmail.com>
2025-05-14 15:54:03 +0530

    Delete passes directory

--- passes/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class PassesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'passes'
-    verbose_name = 'Temple Pass Management'
